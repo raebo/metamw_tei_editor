ea37b206f5c38354280c8d67af5ab1affee400ce
Boris Raeschler <boris.raeschler@googlemail.com>
2026-05-18 10:22:13 +0200

    Local development is now on ssl  using mkcert

--- webpack.config.js
+++ webpack.config.js
@@ -124,9 +124,18 @@ module.exports = (env, argv) => {
             directory: path.resolve(__dirname, 'dist'),
           },
           historyApiFallback: true,
+          host: 'editor.metamw.local',
           hot: true,
           port: 5000,
           open: true,
+          allowedHosts: ['editor.metamw.local'],
+          server: {
+            type: 'https',
+            options: {
+              key:  path.resolve(__dirname, 'certs/editor.metamw.local-key.pem'),
+              cert: path.resolve(__dirname, 'certs/editor.metamw.local.pem'),
+            },
+          }
         }
       : undefined,
   };
